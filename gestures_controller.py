f30f5714cb8191762e820b81274f853c3df932d1
Milk-Cool <milan-pl@mail.ru>
2023-11-30 15:02:11 +0300

    ignore time

--- gestures_controller.py
+++ gestures_controller.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import time
 
 from gestures.down import gesture_is_down
 from gestures.mute import gesture_is_mute
@@ -29,8 +30,11 @@ class GesturesController:
         self.E_PREV = 4
         self.E_UP = 5
 
+        # Time to ignore festures for after one was recognized
+        self.IGNORE_TIME = 0.3
+
         self.last_gesture = self.G_NONE
-        self.last_gesture_time = -1
+        self.last_gesture_time = -100
         self.last_gesture_x = -1
 
         self.detector = mp.solutions.hands.Hands()
@@ -54,12 +58,18 @@ class GesturesController:
         hands = self.get_camera_hands()
         if hands.multi_hand_landmarks is None:
             return
+        
+        ctime = time.time()
+
+        if ctime - self.last_gesture_time < self.IGNORE_TIME:
+            return self.E_NONE
 
         for i in range(len(hands.multi_hand_landmarks)):
             args = [hands.multi_hand_landmarks[i].landmark, hands.multi_handedness[i].classification[0].label]
             if gesture_is_pause(*args):
                 if self.last_gesture != self.G_PAUSE:
                     self.last_gesture = self.G_PAUSE
+                    self.last_gesture_time = ctime
                     return self.E_PAUSE
                 return self.E_NONE
             self.last_gesture = self.G_NONE
