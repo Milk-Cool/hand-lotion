0e00db05e78fa121f9dea4c6b936f12a51df6447
Milk-Cool <milan-pl@mail.ru>
2023-12-04 13:33:32 +0300

    removed code duplication

--- gestures/helpers/finger_folded.py
+++ gestures/helpers/finger_folded.py
@@ -8,48 +8,36 @@ lm = mp.solutions.hands.HandLandmark
 X_ACCURACY = config.FOLD_X_ACCURACY
 MIN_LENGTH = config.FOLD_MIN_LENGTH
 
-# TODO: remove code duplication
+
+def single_finger_folded_straight(gesture, mcp, pip, dip, tip):
+    return (
+        abs(gesture[mcp].x -
+            gesture[pip].x) < X_ACCURACY
+        and abs(gesture[dip].x - gesture[tip].x) < X_ACCURACY
+    )
+
+
+def single_finger_folded_side(gesture, pip, dip):
+    return (
+        abs(gesture[pip].x - gesture[dip].x) < X_ACCURACY
+    )
 
 
 def four_fingers_folded(gesture):
     return (
         # Fingers folded, palm parallel to the camera
-        (
-            abs(gesture[lm.INDEX_FINGER_MCP].x -
-                gesture[lm.INDEX_FINGER_PIP].x) < X_ACCURACY
-            and abs(gesture[lm.INDEX_FINGER_DIP].x - gesture[lm.INDEX_FINGER_TIP].x) < X_ACCURACY
-        )
-        and (
-            abs(gesture[lm.MIDDLE_FINGER_MCP].x -
-                gesture[lm.MIDDLE_FINGER_PIP].x) < X_ACCURACY
-            and abs(gesture[lm.MIDDLE_FINGER_DIP].x - gesture[lm.MIDDLE_FINGER_TIP].x) < X_ACCURACY
-        )
-        and (
-            abs(gesture[lm.RING_FINGER_MCP].x -
-                gesture[lm.RING_FINGER_PIP].x) < X_ACCURACY
-            and abs(gesture[lm.RING_FINGER_DIP].x - gesture[lm.RING_FINGER_TIP].x) < X_ACCURACY
-        )
-        and (
-            abs(gesture[lm.PINKY_MCP].x - gesture[lm.PINKY_PIP].x) < X_ACCURACY
-            and abs(gesture[lm.PINKY_DIP].x - gesture[lm.PINKY_TIP].x) < X_ACCURACY
-        )
+        single_finger_folded_straight(
+            gesture, lm.INDEX_FINGER_MCP, lm.INDEX_FINGER_PIP, lm.INDEX_FINGER_DIP, lm.INDEX_FINGER_TIP)
+        and single_finger_folded_straight(gesture, lm.MIDDLE_FINGER_MCP, lm.MIDDLE_FINGER_PIP, lm.MIDDLE_FINGER_DIP, lm.MIDDLE_FINGER_TIP)
+        and single_finger_folded_straight(gesture, lm.RING_FINGER_MCP, lm.RING_FINGER_PIP, lm.RING_FINGER_DIP, lm.RING_FINGER_TIP)
+        and single_finger_folded_straight(gesture, lm.PINKY_MCP, lm.PINKY_PIP, lm.PINKY_DIP, lm.PINKY_TIP)
     ) or (
         # Fingers folded, palm perpendicular to the camera
-        (
-            abs(gesture[lm.INDEX_FINGER_PIP].x -
-                gesture[lm.INDEX_FINGER_DIP].x) < X_ACCURACY
-        )
-        and (
-            abs(gesture[lm.MIDDLE_FINGER_PIP].x -
-                gesture[lm.MIDDLE_FINGER_DIP].x) < X_ACCURACY
-        )
-        and (
-            abs(gesture[lm.RING_FINGER_PIP].x -
-                gesture[lm.RING_FINGER_DIP].x) < X_ACCURACY
-        )
-        and (
-            abs(gesture[lm.PINKY_PIP].x - gesture[lm.PINKY_DIP].x) < X_ACCURACY
-        )
+        single_finger_folded_side(
+            gesture, lm.INDEX_FINGER_PIP, lm.INDEX_FINGER_DIP)
+        and single_finger_folded_side(gesture, lm.MIDDLE_FINGER_PIP, lm.MIDDLE_FINGER_DIP)
+        and single_finger_folded_side(gesture, lm.RING_FINGER_PIP, lm.RING_FINGER_DIP)
+        and single_finger_folded_side(gesture, lm.PINKY_PIP, lm.PINKY_DIP)
     )
 
 
